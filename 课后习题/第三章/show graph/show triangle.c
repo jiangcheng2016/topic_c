51f3aed77e062075bcf1207ec25d74d706dbb56d
jiangsir <jianglvforever@163.com>
2020-02-29 20:07:26 +0800

    打印三角补充

--- 课后习题/第三章/show graph/show triangle.c
+++ 课后习题/第三章/show graph/show triangle.c
@@ -1,13 +1,25 @@
 #include<stdio.h>
 
 int main()
-{
-	int line,i,j;
+{	
+	/*
+		按行打印三角形，行数从键盘输入，例如行数为5
+						*
+					   ***
+					  *****
+					 *******
+					*********
+	*/
+	int line,i,j,k;
 	printf("请输入行数: \n");
 	scanf("%d",&line);
-	for(i=0;i<line;i++)
+	for(i=1;i<=line;i++)
 	{
-		for(j=0;j<i+line;j++)
+		for(j=0;j<line-i;j++)
+		{
+			printf(" ");
+		}
+		for(k=0;k<2*i-1;k++)
 		{
 			printf("*");
 		}
