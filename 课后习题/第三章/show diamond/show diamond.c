3491f3266a2ea2a2d2accf108efeb2768d82e660
jiangsir <jianglvforever@163.com>
2020-03-02 20:56:02 +0800

    打印菱形

---
+++ 课后习题/第三章/show diamond/show diamond.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int i ,j,k;
+	for(i = 1;i <= 7;i ++)
+	{
+		if(i <= 4)
+		{
+			for(j = 0;j < 4-i;j ++)
+			{
+				printf(" ");
+			}
+			for(k = 0;k < 2*i-1; k ++)
+				printf("*");
+			printf("\n");
+		}
+	}
+	for(i = 7-4;i > 0;i --)
+	{
+		for(j = 0;j < 4-i;j ++)
+		{
+			printf(" ");
+		}
+		for(k = 0;k < 2*i-1; k ++)
+			printf("*");
+		printf("\n");
+	}
+}
