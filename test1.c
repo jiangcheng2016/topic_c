7944e87c033c380171d62aa275aef4532866f79c
jiangsir <jianglvforever@163.com>
2020-03-11 21:59:37 +0800

    test1

---
+++ test1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	/*short a;
+	a=-4;
+	printf("\na:dec=%d,oct=%x,unsigned=%u\n",a,a,a);*/
+
+	int a=5,b=4,c=3;
+	if(a >= b >=c ) c++;
+	printf("%d",c);
+	return 0;
+}
