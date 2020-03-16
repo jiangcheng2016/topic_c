679fa16fdf45111bdc2ca9af421b545a59516685
jiangsir <jianglvforever@163.com>
2020-03-16 17:30:01 +0800

    改动

---
+++ 课后习题/CSDN/showgraph.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	for(i = 1;i < 6;i ++)
+	{
+		for(j = 0;j < 6;j ++)
+		{
+			printf("%d ",i);
+		}
+		printf("\n");
+	}
+	
+
+
+	return 0;
+}
---
+++ 课后习题/CSDN/string_scanf.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stdio.h>
+#include<string>
+#include<cstring>
+
+using namespace std;
+using std::string;
+
+int main()
+{
+	string str;
+	//scanf("%s",&str);
+	cin >> str;
+	cout << str.length() << endl;
+	return 0;
+}
